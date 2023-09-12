bc66ef8e6807d74d81bf2d808c723befdb8b83f0
BryanField15 <4bryanfield@gmail.com>
2023-09-12 20:44:28 +1000

    Task 3 solution

--- 3-script.js
+++ 3-script.js
@@ -3,5 +3,5 @@ document.getElementById("toggle_header").addEventListener("click", toggleColour)
 function toggleColour() {
   const header = document.querySelector("header");
   header.classList.toggle("red");
-  header.classList.toggle("green")
+  header.classList.toggle("green");
 }
